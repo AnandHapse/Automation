947d1f67d854cefae25784f7e98038219a18f38a
AnandHapse <anand.hapse27@gmail.com>
2026-08-29 22:36:41 +0530

    Added Chapter Satement Switch

---
+++ 05_Chapter_Statement/25_IQ_Statement.js
@@ -0,0 +1,11 @@
+let age = 16;
+
+
+if (age > 18) {
+
+    console.log("Go Goa");
+
+} else {
+    
+    console.log("Not Goa");
+}
---
+++ 05_Chapter_Statement/26_IQ_Statement_02.js
@@ -0,0 +1,19 @@
+let Age = 27;
+
+if (Age > 18) {
+
+    console.log("Go Goa");
+
+    if (Age > 26) {
+
+        console.log("Drink");
+    }
+    
+    else {
+
+        console.log("Not Drink");
+    }
+} else {
+
+    console.log("No Goa");
+}
